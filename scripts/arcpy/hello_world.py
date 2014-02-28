9e8503d07b664c00ef2f71b3c053c1670de0a6f6
js174 <js174@6bfcf819-cacf-47a1-ae15-21b88a40d8fc>
2014-02-28 19:00:17 +0000

    HelloWorld and ProgressBar with ArcPy
    git-svn-id: https://ad-svn.informatik.uni-freiburg.de/forestmaps@65 6bfcf819-cacf-47a1-ae15-21b88a40d8fc

--- scripts/arcpy/hello_world.py
+++ scripts/arcpy/hello_world.py
@@ -1,21 +1,30 @@
 import arcpy
 import numpy
 import sys
+import time
 
-assert len(sys.argv) > 3
+#assert len(sys.argv) > 3
 
 print 'Hello World!'
 
 arcpy.AddMessage('Hello Arc-World!')
+arcpy.AddIDMessage('INFORMATIVE', 12, 'Hello World!')
+
+
+arcpy.SetProgressor("step", "Computing...", 0, 60, 1)
+for i in range(60):
+  time.sleep(0.2)
+  arcpy.SetProgressorPosition()
+arcpy.ResetProgressor()
 
 ''' arcpy.da = data access '''
 input = 'c:/path/to/data.gdb/SUB/PATH'
-arr = arcpy.da.FeatureClassToNumPyArray(input, ('COLUMN_NAME1', 'COLUMN_NAME2'))
+#arr = arcpy.da.FeatureClassToNumPyArray(input, ('COLUMN_NAME1', 'COLUMN_NAME2'))
 
-arr2 = arcpy.da.TableToNumPyArray(input, (sys.argv[2], sys.argv[3]))
+#arr2 = arcpy.da.TableToNumPyArray(input, (sys.argv[2], sys.argv[3]))
 
 
-for elem in arr:
-  pass
+#for elem in arr:
+#  pass
 
 
