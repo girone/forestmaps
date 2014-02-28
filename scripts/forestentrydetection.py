69757222658fa91ddc5195dc76989c76bdc0510e
js174 <js174@6bfcf819-cacf-47a1-ae15-21b88a40d8fc>
2014-02-28 19:00:35 +0000

    cosmetics
    git-svn-id: https://ad-svn.informatik.uni-freiburg.de/forestmaps@90 6bfcf819-cacf-47a1-ae15-21b88a40d8fc

--- scripts/forestentrydetection.py
+++ scripts/forestentrydetection.py
@@ -15,7 +15,6 @@ Copyright 2013: Institut fuer Informatik
 Author: Jonas Sternisko <sternis@informatik.uni-freiburg.de>
 
 """
-
 from PIL import Image, ImageDraw
 import sys
 import os.path
@@ -32,253 +31,224 @@ visualize = True
 
 
 def width_and_height(bbox):
-  return (bbox[1][0] - bbox[0][0]), (bbox[1][1] - bbox[0][1])
+    return (bbox[1][0] - bbox[0][0]), (bbox[1][1] - bbox[0][1])
 
 
-def classify(highway_nodes, nodes, grid):
-  ''' Classifies highway nodes whether they are in the forest or on open
-      terrain.
-  '''
-  forestal_highway_nodes = set()
-  open_highway_nodes = set()
-  for node_id in highway_nodes:
-    lon, lat = nodes[node_id]
-    if grid.test((lon, lat)):
-      forestal_highway_nodes.add(node_id)
+def classify(highwayNodes, nodes, grid):
+    """Classifies nodes whether they are in the forest or on open terrain."""
+    forestHighwayNodes = set()
+    openHighwayNodes = set()
+    for nodeId in highwayNodes:
+        lon, lat = nodes[nodeId]
+        if grid.test((lon, lat)):
+            forestHighwayNodes.add(nodeId)
+        else:
+            openHighwayNodes.add(nodeId)
+    return forestHighwayNodes, openHighwayNodes
+
+
+def select_wep(openHighwayNodes, forestHighwayNodes, graph, osmIdMap):
+    """Selects nodes as FEP which are outside the forest and point into."""
+    weps = set()
+    inverseIdMap = {value : key for (key, value) in osmIdMap.items()}
+    for osmId in openHighwayNodes:
+        nodeId = osmIdMap[osmId]
+        for otherNodeId in graph.edges[nodeId].keys():
+            otherOsmId = inverseIdMap[otherNodeId]
+            if otherOsmId in forestHighwayNodes:
+                weps.add(osmId)
+                break
+    return weps
+
+
+def create_population_grid(boundaryPolygons, forestPolygons, resolution=None,
+                           gridPointDistance=None):
+    """Creates the population grid points.
+
+    Points are distributed in a rectangular grid inside the area of the
+    boundaryPolygons minus the forestPolygons. The resolution can be specified
+    either by the number of points along the smaller side of the bounding box
+    of all polygons, or by the distance between every two points.
+
+    """
+    bbox = bounding_box(boundaryPolygons)
+    gridPoints = create_grid_points(bbox, resolution, gridPointDistance)
+    if len(boundaryPolygons) == 1:
+        polys = [boundaryPolygons]
     else:
-      open_highway_nodes.add(node_id)
-  return forestal_highway_nodes, open_highway_nodes
+        polys = boundaryPolygons
+    gridPoints = filter_point_grid(gridPoints, polys, 'intersect')
+    gridPoints = filter_point_grid(gridPoints, forestPolygons, 'difference')
+    return gridPoints
 
 
-def select_wep(open_highway_nodes, forestal_highway_nodes, graph, osm_id_map):
-  ''' Selects nodes as WEP which are outside the forest and point into it. '''
-  weps = set()
-  inverse_id_map = {value : key for (key, value) in osm_id_map.items()}
-  for osm_id in open_highway_nodes:
-    node_id = osm_id_map[osm_id]
-    for other_node_id in graph.edges[node_id].keys():
-      other_osm_id = inverse_id_map[other_node_id]
-      if other_osm_id in forestal_highway_nodes:
-        weps.add(osm_id)
-        break
-  return weps
+def create_grid_points(bbox, resolution, gridPointDistance):
+    """Creates a point grid.
 
+    Creates a point grid for a region with @resolution many points along the
+    smaller side of @bbox or @gridPointDistance meters between every pair of
+    neighboring points.
 
-def create_population_grid(boundary_polygons,
-                           forest_polygons,
-                           resolution=None,
-                           grid_point_distance=None):
-  ''' Creates the population grid points.
-
-  Points are distributed in a rectangular grid inside the area of the
-  boundary_polygons minus the forest_polygons. The resolution can be specified
-  either by the number of points along the smaller side of the bounding box of
-  all polygons, or by the distance between every two points.
-
-  '''
-  bbox = bounding_box(boundary_polygons)
-  grid_points = create_grid_points(bbox, resolution, grid_point_distance)
-  grid_points = filter_point_grid(
-      grid_points,
-      [boundary_polygons] if len(boundary_polygons) == 1 else boundary_polygons,
-      'intersect')
-  grid_points = filter_point_grid(grid_points, forest_polygons, 'difference')
-  return grid_points
-
-
-def create_grid_points(bbox, resolution, grid_point_distance):
-  ''' Creates a point grid for a region with @resolution many points along the
-      smaller side of @bbox or @grid_point_distance meters between every pair of
-      neighboring points.
-  '''
-  assert bool(resolution) != bool(grid_point_distance)  # Specify exactly one!
-  w, h = width_and_height(bbox)
-  min_side = min(w, h)
-  xmin, ymin = bbox[0]
-  if resolution:
-    step = min_side / (resolution + 1)
-  else:
-    if w < 180:
-      # we are working on (lat, lon) coordinates, determine radial step size
-      p0, p1 = bbox
-      p1_p = [p1[0], p0[1]]  # project p1 to same latitude as p0
-      step = (grid_point_distance / great_circle_distance(p0, p1_p)) * w
-      print "step = {0}".format(step)
+    """
+    assert bool(resolution) != bool(gridPointDistance)  # Specify exactly one!
+    w, h = width_and_height(bbox)
+    minSideLength = min(w, h)
+    xmin, ymin = bbox[0]
+    if resolution:
+        step = minSideLength / (resolution + 1)
     else:
-      # Gauss-Kruger (east, north) coordinates in meters
-      step = grid_point_distance
-  grid_points = []
-  for i in range(1, int(math.ceil(h / step))):
-    for j in range(1, int(math.ceil(w / step))):
-      grid_points.append((j*step + xmin, i*step + ymin))  # (lon,lat) ~ (x,y)
-  return grid_points
+        if w < 180:
+            # we are working on (lat, lon) coordinates, determine step size
+            p0, p1 = bbox
+            p1p = [p1[0], p0[1]]  # project p1 to same latitude as p0
+            step = (gridPointDistance / great_circle_distance(p0, p1p)) * w
+            print "step = {0}".format(step)
+        else:
+            # Gauss-Kruger (east, north) coordinates in meters
+            step = gridPointDistance
+    gridPoints = []
+    for i in range(1, int(math.ceil(h / step))):
+        for j in range(1, int(math.ceil(w / step))):
+            # (lon,lat) ~ (x,y)
+            gridPoints.append((j*step + xmin, i*step + ymin))
+    return gridPoints
 
 
 def filter_point_grid(points, regions, operation='intersect'):
-  '''
-  Filters @points by applying @operation with @regions using a grid.
-  Operations:
-    'intersect' : returns @points which lie inside @regions
-    'difference': returns @points which do not lie inside @regions
-  '''
-  assert operation in ['intersect', 'difference']
-  bbox = bounding_box(points + [p for region in regions for p in region])
-  bbox = (bbox[0], [bbox[1][0] * 1.01, bbox[1][1]*1.01])
-  grid = Grid(bbox, grid_size=(1024, 860))
-  for poly in regions:
-    grid.fill_polygon(poly)
-  if operation is 'intersect':
-    return [p for p in points if grid.test(p) != 0]
-  elif operation is 'difference':
-    return [p for p in points if grid.test(p) == 0]
-  else:
-    print "Error: Unsupported operation for 'filter_point_grid'."
-    exit(1)
+    """Filters @points by applying @operation with @regions using a grid.
+
+    Operations:
+      'intersect' : returns @points which lie inside @regions
+      'difference': returns @points which do not lie inside @regions
+
+    """
+    assert operation in ['intersect', 'difference']
+    bbox = bounding_box(points + [p for region in regions for p in region])
+    bbox = (bbox[0], [bbox[1][0] * 1.01, bbox[1][1]*1.01])
+    grid = Grid(bbox, grid_size=(1024, 860))
+    for poly in regions:
+        grid.fill_polygon(poly)
+    if operation is 'intersect':
+        return [p for p in points if grid.test(p) != 0]
+    elif operation is 'difference':
+        return [p for p in points if grid.test(p) == 0]
+    else:
+        print "Error: Unsupported operation for 'filter_point_grid'."
+        exit(1)
 
 
-def classify_forest(node_ids, ways_by_type, graph, nodes, osm_id_map,
-                    filename_base):
-  ''' Creates forest polygons and detects forest entries WE in the data. '''
-  forest_delim = ways_by_type['forest_delim']
-  bbox = bounding_box(nodes.values())
-  print bbox
+def classify_forest(nodeIds, waysByType, graph, nodes, osmIdMap, filenameBase):
+    """Creates forest polygons and detects forest entries WE in the data."""
+    forestDelim = waysByType['forest_delim']
+    bbox = bounding_box(nodes.values())
+    print bbox
 
-  print 'Computing the convex hull...'
-  if visualize:
-    visual_grid = Grid(bbox, mode="RGB")
-  boundary_filename = filename_base + ".boundary.out"
-  if os.path.exists(boundary_filename):
-    hull = convexhull.load(boundary_filename)
-  else:
-    points = [list(p) for p in nodes.values()]
-    hull = convexhull.compute(points)
-    convexhull.save(hull, boundary_filename)
-  if visualize:
-    visual_grid.fill_polygon(hull, fill="#fadbaa")
+    print "Computing the convex hull..."
+    if visualize:
+        visualGrid = Grid(bbox, mode="RGB")
+    boundaryFilename = filenameBase + ".boundary.out"
+    if os.path.exists(boundaryFilename):
+        hull = convexhull.load(boundaryFilename)
+    else:
+        points = [list(p) for p in nodes.values()]
+        hull = convexhull.compute(points)
+        convexhull.save(hull, boundaryFilename)
+    if visualize:
+        visualGrid.fill_polygon(hull, fill="#fadbaa")
 
-  print 'Creating forest grid from polygons...'
-  forest_polygons = \
-      [[nodes[id] for id in node_ids[way_id]] for way_id in forest_delim]
-  forest_grid = Grid(bbox)
-  for poly in forest_polygons:
-    forest_grid.fill_polygon(poly)
+    print "Creating forest grid from polygons..."
+    forestPolygons = [[nodes[id] for id in nodeIds[wayId]]
+                      for wayId in forestDelim]
+    forestGrid = Grid(bbox)
+    for poly in forestPolygons:
+        forestGrid.fill_polygon(poly)
 
-  print 'Classifying highway nodes...'
-  highway_node_ids = \
-      set([id for way_id in ways_by_type['highway'] for id in node_ids[way_id]])
-  forestal_highway_nodes, open_highway_nodes = \
-      classify(highway_node_ids, nodes, forest_grid)
+    print "Classifying highway nodes..."
+    highwayNodeIds = set([id for wayId in waysByType['highway']
+                             for id in nodeIds[wayId]])
+    forestHighwayNodes, openHighwayNodes = classify(highwayNodeIds, nodes,
+                                                    forestGrid)
 
-  print 'Restrict forests to large connected components...'
-  # turn this off, when fast results are needed
-  node_idx = [osm_id_map[e] for e in forestal_highway_nodes]
-  node_idx, removed = graph.filter_components(node_idx, 500)
-  inverse_id_map = {value : key for (key, value) in osm_id_map.items()}
-  forestal_highway_nodes = set([inverse_id_map[e] for e in node_idx])
-  open_highway_nodes.union(set([inverse_id_map[e] for e in removed]))
+    print "Restrict forests to large connected components..."
+    # turn this off, when fast results are needed
+    nodeIdx = [osmIdMap[e] for e in forestHighwayNodes]
+    nodeIdx, removed = graph.filter_components(nodeIdx, 500)
+    inverseIdMap = {value : key for (key, value) in osmIdMap.items()}
+    forestHighwayNodes = set([inverseIdMap[e] for e in nodeIdx])
+    openHighwayNodes.union(set([inverseIdMap[e] for e in removed]))
 
-  nodeinfo = {osm_id_map[osm_id] : NodeInfo(osm_id, nodes[osm_id])
-              for osm_id in highway_node_ids}
+    nodeinfo = {osmIdMap[osmId] : NodeInfo(osmId, nodes[osmId])
+                for osmId in highwayNodeIds}
 
-  # This does not work properly and does not bring the expected benefit.
-  #print 'Restrict the graph to largest connected component...'
-  #n = graph.size()
-  #c = len(graph.nodes)
-  #graph = graph.lcc()
-  #print "Removed " + str(c - len(graph.nodes))
-  ## update indices
-  #delete = np.array([0]*n)
-  #for i in range(1, n):
-  #  if i not in graph.nodes:
-  #    delete[i] = 1
-  #index_shift = np.cumsum(delete)
-  #shifted_graph = Graph()
-  #for node in graph.nodes:
-  #  if delete[node] == 0:
-  #    for to, edge in graph.edges[node].items():
-  #      if delete[to] == 0:
-  #        shifted_graph.add_edge(node - index_shift[node],
-  #            to - index_shift[to], edge.cost)
-  #graph = shifted_graph
-  #osm_id_map = \
-  #    {k : v - index_shift[v] for k, v in osm_id_map.items() if delete[v] == 0}
-  #ind = set([inverse_id_map[n] for n in graph.nodes if delete[node] == 0])
-  #forestal_highway_nodes &= ind
-  #open_highway_nodes &= ind
-  #print 'Removed %d nodes, new size is %d.' % (n - graph.size(),
-  #    graph.size())
+    print "Select WEPs..."
+    weps = select_wep(openHighwayNodes, forestHighwayNodes, graph, osmIdMap)
+    print str(len(weps)) + ' WEPs'
 
-  print 'Select WEPs...'
-  weps = select_wep(open_highway_nodes, forestal_highway_nodes, graph,
-      osm_id_map)
-  print str(len(weps)) + ' WEPs'
+    print "Creating the population grid..."
+    xmin, ymin = bbox[0]
+    xmax, ymax = bbox[1]
+    dummy = [(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax)]
+    boundaryPolygon = hull
+    population = create_population_grid(boundaryPolygon, forestPolygons, 10)
 
-  print 'Creating the population grid...'
-  xmin, ymin = bbox[0]
-  xmax, ymax = bbox[1]
-  dummy = [(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax)]
-  boundary_polygon = hull
-  population = create_population_grid(boundary_polygon, forest_polygons, 10)
+    if visualize:
+        print "Visualizing the result..."
+        for poly in forestPolygons:
+            visualGrid.fill_polygon(poly, fill="#00DD00")
+        for nodeId in weps:
+            (x,y) = visualGrid.transform(nodes[nodeId])
+            r = 10
+            visualGrid.draw.ellipse((x-r,y-r,x+r,y+r), fill="#BB1111")
+        for (x,y) in population:
+            (x,y) = visualGrid.transform((x,y))
+            r = 30
+            visualGrid.draw.ellipse((x-r, y-r, x+r, y+r), fill="#0000FF")
+        visualGrid.show()
 
-  if visualize:
-    print 'Visualizing the result...'
-    for poly in forest_polygons:
-      visual_grid.fill_polygon(poly, fill="#00DD00")
-    for node_id in weps:
-      x, y = visual_grid.transform(nodes[node_id])
-      r = 10
-      visual_grid.draw.ellipse((x-r,y-r,x+r,y+r), fill="#BB1111")
-    for (x,y) in population:
-      (x, y) = visual_grid.transform((x,y))
-      r = 30
-      visual_grid.draw.ellipse((x-r, y-r, x+r, y+r), fill="#0000FF")
-    visual_grid.show()
-
-  # restrict to used nodes
-  used_osm_ids = forestal_highway_nodes | open_highway_nodes
-  nodes = {k:v for k,v in nodes.items() if k in used_osm_ids}
-  return weps, forestal_highway_nodes, population, nodeinfo
+    # restrict to used nodes
+    usedOsmIds = forestHighwayNodes | openHighwayNodes
+    nodes = {k:v for k,v in nodes.items() if k in usedOsmIds}
+    return weps, forestHighwayNodes, population, nodeinfo
 
 
 def main():
-  if len(sys.argv) < 2 or os.path.splitext(sys.argv[1])[1] != '.osm':
-    print ''' No osm file specified! '''
-    exit(1)
-  std_osm = True
-  if "ATKIS" in sys.argv:
-    std_osm = False
-    sys.argv.remove("ATKIS")
-    print ' - Using ATKIS interpreter.'
+    if len(sys.argv) < 2 or os.path.splitext(sys.argv[1])[1] != '.osm':
+        print "No osm file specified!"
+        exit(1)
+    standardOSM = True
+    if "ATKIS" in sys.argv:
+        standardOSM = False
+        sys.argv.remove("ATKIS")
+        print " - Using ATKIS interpreter."
 
-  osmfile = sys.argv[1]
-  maxspeed = int(sys.argv[2]) if len(sys.argv) > 2 else 130
+    osmfile = sys.argv[1]
+    maxspeed = int(sys.argv[2]) if len(sys.argv) > 2 else 130
 
-  print 'Reading nodes and ways from OSM and creating the graph...'
-  node_ids, ways_by_type, graph, nodes, osm_id_map = osm_parse.read_file(
-      osmfile, maxspeed, interpret=osm_parse.Std_OSM_way_tag_interpreter
-      if std_osm else osm_parse.ATKIS_to_OSM_way_tag_interpreter)
+    print "Reading nodes and ways from OSM and creating the graph..."
+    iterpreter = (osm_parse.Std_OSM_way_tag_interpreter if standardOSM
+                  else osm_parse.ATKIS_to_OSM_way_tag_interpreter)
+    tmp = osm_parse.read_file(osmfile, maxspeed, iterpreter)
+    (nodeIds, waysByType, graph, nodes, osmIdMap) = tmp
 
-  print len(graph.nodes), graph.size(), sum([len(edges) for edges in
-    graph.edges.values()])
+    print (len(graph.nodes), graph.size(),
+           sum([len(edges) for edges in graph.edges.values()]))
 
-  filename_base = os.path.splitext(osmfile)[0]
-  weps, forestal_highway_nodes, population, nodeinfo = classify_forest(
-      node_ids, ways_by_type, graph, nodes, osm_id_map, filename_base)
+    filenameBase = os.path.splitext(osmfile)[0]
+    tmp = classify_forest(nodeIds, waysByType, graph, nodes, osmIdMap,
+                          filenameBase)
+    (weps, forestHighwayNodes, population, nodeinfo) = tmp
 
-  print 'Writing output...'
-  filename = filename_base + "." + str(maxspeed) + "kmh"
-  for data, extension in zip(
-      [weps, forestal_highway_nodes, population, graph, osm_id_map, nodes,
-       nodeinfo],
-      ['weps', 'forest_ids', 'population', 'graph', 'id_map', 'nodes',
-       'nodeinfo']
-      ):
-    f = open(filename + "." + extension + ".out", 'w')
-    pickle.dump(data, f, protocol=2)
-    f.close()
+    print "Writing output..."
+    filename = filenameBase + "." + str(maxspeed) + "kmh"
+    zipped = zip(
+    [weps, forestHighwayNodes, population, graph, osmIdMap, nodes, nodeinfo],
+    ['Feps', 'ForestIds', 'Population', 'Graph', 'IdMap', 'Nodes', 'Nodeinfo'])
+    for data, extension in zipped:
+        f = open(filename + "." + extension + ".out", 'w')
+        pickle.dump(data, f, protocol=2)
+        f.close()
 
 
 if __name__ == '__main__':
-  ''' Run this module '''
-  main()
+    main()
 
