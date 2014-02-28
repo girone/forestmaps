4d9f7b9ccb563c868e611e8f520751a8bb8966c2
js174 <js174@6bfcf819-cacf-47a1-ae15-21b88a40d8fc>
2014-02-28 19:00:25 +0000

    util checks for arcpy on load, can be used without
    git-svn-id: https://ad-svn.informatik.uni-freiburg.de/forestmaps@76 6bfcf819-cacf-47a1-ae15-21b88a40d8fc

--- scripts/arcpy/util.py
+++ scripts/arcpy/util.py
@@ -1,10 +1,16 @@
 ''' util.py : Contains helper functions. '''
 import time
-import arcpy
+try:
+  import arcpy
+  HAVE_ARCPY = True
+except:
+  print "Warning from util.py: Could not find module 'arcpy'!"
+  HAVE_ARCPY = False
 
 
 def msg(text):
-  arcpy.AddMessage(text)
+  if HAVE_ARCPY:
+    arcpy.AddMessage(text)
   print text
 
 
