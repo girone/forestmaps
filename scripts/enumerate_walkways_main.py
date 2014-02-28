6656c45fa16c82d3e5edf6471658f41cf32da8ea
js174 <js174@6bfcf819-cacf-47a1-ae15-21b88a40d8fc>
2014-02-28 19:00:08 +0000

    Fixed usage of node contraction and restriction to forest
    git-svn-id: https://ad-svn.informatik.uni-freiburg.de/forestmaps@53 6bfcf819-cacf-47a1-ae15-21b88a40d8fc

--- scripts/enumerate_walkways_main.py
+++ scripts/enumerate_walkways_main.py
@@ -26,28 +26,32 @@ def main():
   g = pickle.load(open(sys.argv[4]))
   nodeinfo = pickle.load(open(sys.argv[5]))
 
-  ''' concentrate on the inner forests '''
+  wep_nodes = [osm_id_map[osm_id] for osm_id in weps]
+  wep_nodes_set = set(wep_nodes)
+
+  ''' Concentrate way generation on forests '''
   n = len(g.nodes)
   outside_nodes = g.nodes - set([osm_id_map[id] for id in forest_nodes_osm_ids])
-  outside_nodes -= set([osm_id_map[wep] for wep in weps])
+  outside_nodes -= wep_nodes_set
   g.remove_partition(outside_nodes)
   print n, len(g.nodes)
 
+  ''' Contract binary nodes except WEPs. '''
+  n = len(g.nodes)
+  g.contract_binary_nodes(exclude=wep_nodes)
+  print n, len(g.nodes)
 
-  ''' generate the walkways from every wep to all weps (within distance) '''
-  wep_nodes = [osm_id_map[osm_id] for osm_id in weps]
-  wep_nodes_set = set(wep_nodes)
+
+  ''' Generate the walkways from every wep to all weps (within distance) '''
   count = 0
   for node in wep_nodes:
     walkways = enumerate_walkways(g, node, target_nodes=wep_nodes_set, \
-        cost_limit=5*60, local_cycle_depth=5)
-    print walkways
+        cost_limit=15*60, local_cycle_depth=5)
+    print "%d of %d ways generated" % (count, len(wep_nodes))
+    #print walkways
     print str(len(walkways)) + " many ways found with cost limit 5min."
-    s = raw_input("Press ENTER for next cycle.")
+    #s = raw_input("Press ENTER for next cycle.")
 
-  ''' DEBUG '''
-  visual_grid.visualize_nodes(g, nodeinfo, set([307433, 140536, 140535]), \
-      inset=1.0, forest=forest_polygons)
 
 if __name__ == '__main__':
   main()
