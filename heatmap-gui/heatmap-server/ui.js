04e1130c2dc2476a348434ea0740a87164eaf76d
js174 <js174@6bfcf819-cacf-47a1-ae15-21b88a40d8fc>
2014-02-28 19:01:22 +0000

    Adds firefox warning and loading animation
    git-svn-id: https://ad-svn.informatik.uni-freiburg.de/forestmaps@129 6bfcf819-cacf-47a1-ae15-21b88a40d8fc

--- heatmap-gui/heatmap-server/ui.js
+++ heatmap-gui/heatmap-server/ui.js
@@ -230,6 +230,17 @@ window.onload = function() {
 };
 
 
+// Loading animation
+$(document).on({
+  ajaxStart: function() { 
+    $("body").addClass("loading"); 
+  },
+  ajaxStop: function() { 
+    $("body").removeClass("loading"); 
+  }    
+});
+
+
 $(document).ready(function(){
     allowCentering = true
     lastRequestTimeStamp = 0;
